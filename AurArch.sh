5f20fa344ed69dbbce378f54d9de868a4f84e76b
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-19 03:12:40 -0700

    Update AurArch.sh

--- AurArch.sh
+++ AurArch.sh
@@ -9,52 +9,87 @@ current_user=$(whoami)
 # Welcome message
 echo "Hello $current_user"
 echo "What are we doing today?"
-echo "Just Cloning For Now"
-echo "I'll Be Cloning and Installing"
 
-# Get AUR repository URL from the user
-read -p "Enter AUR Repository URL: " aur_repo_url
+options=("Just Cloning For Now" "I'll Be Cloning and Installing")
 
-# Verify the input is not empty
-if [ -z "$aur_repo_url" ]; then
-    echo "Repository URL cannot be empty. Exiting."
-    exit 1
-fi
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Just Cloning For Now")
+            echo "You chose to just clone for now."
+            
+            # Get AUR repository URL from the user
+            read -p "Enter AUR Repository URL: " aur_repo_url
 
-# Extract the name of the repository from the URL
-repo_name=$(basename "$aur_repo_url" .git)
+            # Verify the input is not empty
+            if [ -z "$aur_repo_url" ]; then
+                echo "Repository URL cannot be empty. Exiting."
+                exit 1
+            fi
 
-# Set the chosen directory to the Git-Clones directory in the home folder
-chosen_directory="~/Git-Clones/$repo_name"
+            # Extract the name of the repository from the URL
+            repo_name=$(basename "$aur_repo_url" .git)
 
-# Clone the repository
-git clone "$aur_repo_url" "$chosen_directory" &
+            # Set the chosen directory to the Git-Clones directory in the home folder
+            chosen_directory="~/Git-Clones/$repo_name"
 
-# Navigate to the downloaded directory
-cd "$chosen_directory" || exit 1
+            # Clone the repository
+            git clone "$aur_repo_url" "$chosen_directory" &
+            
+            break
+            ;;
+        "I'll Be Cloning and Installing")
+            echo "You chose to clone and install."
+            
+            # Get AUR repository URL from the user
+            read -p "Enter AUR Repository URL: " aur_repo_url
 
-# Build the AUR package
-makepkg -s &
+            # Verify the input is not empty
+            if [ -z "$aur_repo_url" ]; then
+                echo "Repository URL cannot be empty. Exiting."
+                exit 1
+            fi
 
-# Check if the build was successful
-wait $!
-if [ $? -eq 0 ]; then
-    # Ask the user if they want to copy the folder to .config
-    read -p "Would you like to copy this folder to .config? (yes/no): " choice
-    if [ "$choice" = "yes" ]; then
-        cp -r "$chosen_directory" "$HOME/.config/$(basename "$chosen_directory")" &
-        wait $!
-        echo "Folder copied to .config successfully."
-    else
-        echo "Folder not copied."
-    fi
-else
-    read -p "Build failed. Would you like to see the build output? (yes/no): " choice
-    if [ "$choice" = "yes" ]; then
-        # Display the build output
-        cat PKGBUILD
-    fi
-fi
+            # Extract the name of the repository from the URL
+            repo_name=$(basename "$aur_repo_url" .git)
+
+            # Set the chosen directory to the Git-Clones directory in the home folder
+            chosen_directory="~/Git-Clones/$repo_name"
+
+            # Clone the repository
+            git clone "$aur_repo_url" "$chosen_directory" &
+
+            # Navigate to the downloaded directory
+            cd "$chosen_directory" || exit 1
+
+            # Build the AUR package
+            makepkg -si
+
+            # Check if the build was successful
+            wait $!
+            if [ $? -eq 0 ]; then
+                # Ask the user if they want to copy the folder to .config
+                read -p "Would you like to copy this folder to .config? (yes/no): " choice
+                if [ "$choice" = "yes" ]; then
+                    cp -r "$chosen_directory" "$HOME/.config/$(basename "$chosen_directory")" &
+                    wait $!
+                    echo "Folder copied to .config successfully."
+                else
+                    echo "Folder not copied."
+                fi
+            else
+                read -p "Build failed. Would you like to see the build output? (yes/no): " choice
+                if [ "$choice" = "yes" ]; then
+                    # Display the build output
+                    cat PKGBUILD
+                fi
+            fi
+            
+            break
+            ;;
+        *) echo "Invalid option $REPLY";;
+    esac
+done
 
 # Wait for user input before exiting
 read -p "Press any key to exit."
