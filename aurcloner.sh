dce0bd1af039a921ad2271eb4ce484917df59e2c
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-19 02:34:16 -0700

    Update aurcloner.sh

--- aurcloner.sh
+++ aurcloner.sh
@@ -28,24 +28,31 @@ git clone "$aur_repo_url" "$chosen_directory"
 # Navigate to the downloaded directory
 cd "$chosen_directory" || exit 1
 
-# Build and install the AUR package
-makepkg -si
+# Build the AUR package
+makepkg -s
 
-# Check if the installation was successful
+# Check if the build was successful
 if [ $? -eq 0 ]; then
-    # Ask the user if they want to copy the folder to .config
-    whiptail --yesno "Would you like to copy this folder to .config?" 10 60 --title "Confirmation"
-    response=$?
+    # Install the AUR package
+    sudo makepkg -si
 
-    # Interpret the user's response
-    if [ $response -eq 0 ]; then
-        cp -r "$chosen_directory" "$HOME/.config/$(basename "$chosen_directory")"
-        whiptail --msgbox "Folder copied to .config successfully." 10 60 --title "Success"
+    # Check if the installation was successful
+    if [ $? -eq 0 ]; then
+        # Ask the user if they want to copy the folder to .config
+        if (whiptail --yesno "Would you like to copy this folder to .config?" 10 60 --title "Confirmation"); then
+            sudo cp -r "$chosen_directory" "$HOME/.config/$(basename "$chosen_directory")"
+            whiptail --msgbox "Folder copied to .config successfully." 10 60 --title "Success"
+        else
+            whiptail --msgbox "Folder not copied." 10 60 --title "Information"
+        fi
     else
-        whiptail --msgbox "Folder not copied." 10 60 --title "Information"
+        whiptail --msgbox "Installation failed. Please check the build output." 10 60 --title "Error"
     fi
 else
-    whiptail --msgbox "Installation failed. Please check the build output." 10 60 --title "Error"
+    if (whiptail --yesno "Build failed. Would you like to see the build output?" 10 60 --title "Build Failed"); then
+        # Display the build output
+        cat PKGBUILD
+    fi
 fi
 
 # Wait for user input before exiting
