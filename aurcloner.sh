442936764983573c2c444632180ba4c9d6856a08
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-19 02:55:32 -0700

    Update aurcloner.sh

--- aurcloner.sh
+++ aurcloner.sh
@@ -7,20 +7,17 @@ mkdir -p ~/Git-Clones
 current_user=$(whoami)
 
 # Welcome message
-whiptail --title "AUR Downloader" --msgbox --scrolltext --center "\
-Hello $current_user
-
-What are we doing today?
-
-1. Just Cloning For Now
-2. I'll Be Cloning and Installing" 25 80
+echo "Hello $current_user"
+echo "What are we doing today?"
+echo "1. Just Cloning For Now"
+echo "2. I'll Be Cloning and Installing"
 
 # Get AUR repository URL from the user
-aur_repo_url=$(whiptail --inputbox "Enter AUR Repository URL:" 10 60 --title "AUR Downloader" 3>&1 1>&2 2>&3)
+read -p "Enter AUR Repository URL: " aur_repo_url
 
 # Verify the input is not empty
 if [ -z "$aur_repo_url" ]; then
-    whiptail --msgbox "Repository URL cannot be empty. Exiting." 10 60 --title "Error" --exitstatus 1
+    echo "Repository URL cannot be empty. Exiting."
     exit 1
 fi
 
@@ -41,23 +38,21 @@ makepkg -s
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
-    # Install the AUR package
-    makepkg -si
-
-    # Check if the installation was successful
-    if [ $? -eq 0 ]; then
-        # Clean up the directory after installation
-        rm -rf "$chosen_directory"
-        whiptail --msgbox "Installation successful and directory cleaned." 10 60 --title "Success"
+    # Ask the user if they want to copy the folder to .config
+    read -p "Would you like to copy this folder to .config? (yes/no): " choice
+    if [ "$choice" = "yes" ]; then
+        cp -r "$chosen_directory" "$HOME/.config/$(basename "$chosen_directory")"
+        echo "Folder copied to .config successfully."
     else
-        whiptail --msgbox "Installation failed. Please check the build output." 10 60 --title "Error"
+        echo "Folder not copied."
     fi
 else
-    if (whiptail --yesno "Build failed. Would you like to see the build output?" 10 60 --title "Build Failed"); then
+    read -p "Build failed. Would you like to see the build output? (yes/no): " choice
+    if [ "$choice" = "yes" ]; then
         # Display the build output
         cat PKGBUILD
     fi
 fi
 
 # Wait for user input before exiting
-whiptail --msgbox "Press OK to exit." 10 60
+read -p "Press any key to exit."
